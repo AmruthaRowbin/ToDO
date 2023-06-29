477ae1857071ba4e64907b6e2bcad0fe34fe3b08
amrutha <amrtha1994@gmail.com>
2023-06-29 19:01:01 +0530

    Add toDo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  // Function to handle deleting a todo
+  const deleteToDo = (id) => {
+    setToDos(toDos.filter((todo) => todo.id !== id));
+  };
+
+  // Function to handle adding a todo
+  const addTodo = () => {
+    if (toDo.trim() !== '') {
+      setToDos([
+        ...toDos,
+        { id: Date.now(), text: toDo, status: false, timestamp: new Date() },
+      ]);
+      setToDo(''); // Clear the input text
+    }
+  };
+
+  // Function to handle checking/unchecking a todo
+  const toggleTodo = (id) => {
+    setToDos(
+      toDos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, status: !todo.status };
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={addTodo} className="fas fa-plus"></i>
+      </div>
+
+      <div className="todos">
+        {toDos.map((obj) => (
+          <div className={obj.status ? 'todo crossed' : 'todo'} key={obj.id}>
+            <div className="left">
+              <input
+                onChange={() => toggleTodo(obj.id)}
+                checked={obj.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{obj.text}</p>
+              <div className="timestamp">{obj.timestamp.toLocaleString()}</div>
+            </div>
+            <div className="right">
+              <i onClick={() => deleteToDo(obj.id)} className="fas fa-times"></i>
+            </div>
+          </div>
+        ))}
+        {toDos
+          .filter((obj) => obj.status)
+          .map((obj) => (
+            <h1 key={obj.id}>{obj.text}</h1>
+          ))}
+      </div>
     </div>
   );
 }
